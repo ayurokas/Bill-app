10b092b8f8ba0abe1af840673c37fc1814f5f7c7
perso <elisa.github@gmail.com>
2023-08-01 21:21:05 +0200

    update test

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -33,26 +33,35 @@ describe("Given I am connected as an employee", () => {
       window.onNavigate(ROUTES_PATH.Bills)
       await waitFor(() => screen.getByTestId('icon-window'))
       const windowIcon = screen.getByTestId('icon-window')
-     // to-do write expect expression
+      // to-do write expect expression
       // Verifie que la class "active-icon" est bien présente sur l'élement avec le data-testid = "icon-window"
+      //vérifie que l'icône des notes de frais dans la mise en page verticale est mise en surbrillance lorsque l'utilisateur est connecté en tant qu'employé.
       expect(windowIcon.classList.contains("active-icon")).toBeTruthy();
     });
 
-    // [1 - Bug report] - Le test Bills est au rouge/FAIL (src/__tests__/Bills.js) / les notes de frais ne s'affichent pas par ordre décroissant.
-    test("Then bills should be ordered from earliest to latest", () => {
-      //effectue un tri des éléments du tableau bills en fonction de leur propriété date. Elle utilise la fonction sort() 
-      //pour organiser les éléments du tableau en fonction du résultat retourné par la fonction de comparaison qui lui est passée en argument. 
-      //Cette fonction de comparaison soustrait les dates b.date et a.date, ce qui entraîne un tri des éléments par ordre décroissant de date.
+     // [1 - Rapport de bug] - Cas de test pour vérifier l'ordre des notes de frais du plus ancien au plus récent
+     test("Alors les notes de frais devraient être triées du plus ancien au plus récent", () => {
+      // Créer l'interface utilisateur des notes de frais avec les données triées en ordre décroissant en fonction de leurs dates
       document.body.innerHTML = BillsUI({ data: bills.sort((a, b) => new Date(b.date) - new Date(a.date)) })
+
+      // Obtenir toutes les dates de l'interface utilisateur et les convertir en un tableau de chaînes de caractères
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
+
+      // Fonction pour trier les dates dans l'ordre inverse (ordre decroissant)
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
+
+      // Créer une copie triée du tableau de dates en utilisant la fonction d'ordre ordre decroissant
       const datesSorted = [...dates].sort(antiChrono)
+
+      // Vérifier si les dates de l'interface utilisateur correspondent aux dates triées
       expect(dates).toEqual(datesSorted)
     })
   });
 
+  //Vérifier que cliquer sur l'icône de l'œil ouvre une fenêtre modale
   describe("When I am on Bills Page and I click on icon eye", () => {
     test("Then a modal should open", () => {
+      // Configuration de l'environnement de test et simulation du type d'utilisateur "Employee" dans le stockage local
       Object.defineProperty(window, localStorage, { value: localStorageMock })
       window.localStorage.setItem("user", JSON.stringify({ type: 'Employee' }))
       document.body.innerHTML = BillsUI({ data: bills })
